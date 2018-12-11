3208a51fac323037e40664a659711e73e118d384
yassine <yassine.snoussi@ssense.com>
2018-12-11 15:11:57 -0500

    original state

--- src/hello.service.interface.ts
+++ src/hello.service.interface.ts
@@ -1,6 +1,6 @@
 export interface IHelloService {
     sayHello(): string;
-     greetings(name: string): string;
-     notPolite(name: string): string;
+    /* greetings(name: string): string;
+     notPolite(name: string): string;*/
 
 }
