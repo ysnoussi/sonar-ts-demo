7c9e34510bce5627188de7afd8fd39ad1a701269
yassine <yassine.snoussi@ssense.com>
2018-12-19 01:39:55 -0500

    add code to drop coverage

--- src/hello.service.interface.ts
+++ src/hello.service.interface.ts
@@ -1,7 +1,7 @@
 export interface IHelloService {
     sayHello(): string;
-   /* sayHi(): string;
+    sayHi(): string;
     greetings(name: string): string;
-    notPolite(name: string): string;*/
+    notPolite(name: string): string;
 
 }
